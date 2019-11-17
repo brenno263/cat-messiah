d4c6902639d3cc11dc4a9859be0f505e36e51f0d
brenno263 <brenno263@gmail.com>
2019-11-17 00:04:14 -0600

    Player Controller, Basic Enemies, Basic Projectiles, Follow Camera

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region variables
+    [Header("Set in Inspector")]
+    public float maxHealth;
+
+    //[Header("Set Dynamically")]
+    private float _health;
+    public float health
+    {
+        get { return _health; }
+        set
+        {
+            _health = Mathf.Min(maxHealth, value);
+            if (_health <= 0) Destroy(gameObject);
+        }
+    }
+	//[header("Fetched on Init")]
+	#endregion
+
+	#region monobehavior methods
+    void Start()
+    {
+        health = maxHealth;
+    }
+    #endregion
+
+    #region private methods
+    public void Damage(float amt)
+    {
+        health -= amt;
+    }
+    #endregion
+}
---
+++ Assets/__Scripts/Projectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    #region variables
+    [Header("Set in Inspector")]
+    public float damage;
+    public float speed;
+	//[header("Set Dynamically")]
+	//[header("Fetched on Init")]
+	#endregion
+
+	#region monobehavior methods
+    void Start()
+    {
+        var rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = transform.TransformDirection(new Vector2(1,0) * speed);
+        awaketime = Time.time;
+    }
+    float awaketime;
+    void Update()
+    {
+        if(Time.time - awaketime > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch(collision.transform.tag)
+        {
+            case "Enemy":
+                OnEnemyHit(collision.transform.GetComponent<Enemy>());
+                Destroy(gameObject);
+                break;
+            default:
+                Destroy(gameObject);
+                break;
+        }
+    }
+    #endregion
+
+    #region private methods
+    protected void OnEnemyHit(Enemy e)
+    {
+        e.Damage(damage);
+    }
+    #endregion
+}
