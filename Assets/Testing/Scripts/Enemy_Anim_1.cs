d25467f3aee976bd6b415519b5d045c44cf65888
brenno263 <brenno263@gmail.com>
2020-02-15 23:53:34 -0600

    Fixing merge conflicts and file architecture

---
+++ Assets/Testing/Scripts/Enemy_Anim_1.cs
@@ -0,0 +1,18 @@
+﻿using __Scripts;
+using UnityEngine;
+
+namespace Testing.Scripts
+{
+    public class Enemy_Anim_1 : MonoBehaviour
+    {
+        public Animator animator;
+
+        // Update is called once per frame
+
+        private void Start()
+        {
+            Enemy enemy = GetComponent<Enemy>();
+            enemy.onDeath = () => { animator.SetTrigger("Death"); };
+        }
+    }
+}
