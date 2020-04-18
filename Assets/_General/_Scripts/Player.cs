4ce6339abf371392f049dc2a7d2e47cc6642289c
Brennan Seymour <brenno263@gmail.com>
2020-04-17 23:24:26 -0500

    Level 1 start

---
+++ Assets/_General/_Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace _General._Scripts
+{
+	public class Player : MonoBehaviour
+	{
+		#region variables
+
+		//[Header("Set in Inspector")]
+
+		[Header("Set Dynamically")]
+		public PlayerState PlayerState;
+		//[Header("Fetched on Init")]
+		#endregion
+
+		#region monobehavior methods
+
+		void Start()
+		{
+		}
+
+		void Update()
+		{
+			
+		}
+
+		#endregion
+
+		#region private methods
+
+		#endregion
+	}
+}
