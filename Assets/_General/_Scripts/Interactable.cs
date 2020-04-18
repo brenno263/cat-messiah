3c41d05ae4104bf84648a8cad048c2e7742b1f76
Brennan Seymour <brenno263@gmail.com>
2020-04-18 14:25:05 -0500

    fixes

---
+++ Assets/_General/_Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InteractionType
+{
+	Item, StairsUp, StairsDown, Environment
+}
+
+public class Interactable : MonoBehaviour
+{
+	#region variables
+
+	[Header("Set in Inspector")]
+	public InteractionType type;
+
+	public Action onInteract;
+	//[Header("Set Dynamically")]
+	//[Header("Fetched on Init")]
+	#endregion
+
+	#region monobehavior methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region private methods
+    #endregion
+}
